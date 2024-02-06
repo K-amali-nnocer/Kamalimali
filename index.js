34b0902df66c4a383398657b581728f3141defb3
KAMAL INNOCENT <158250453+K-amali-nnocer@users.noreply.github.com>
2024-02-06 12:01:40 +0300

    Add files via upload
    This is my Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let userDetails = null; //Global declaration
+function login(userName, password){
+    userDetails = "2200704408@gmail.com", "22/U/4408/EVE@2024";
+    return console.log("User email is " + userName + ", and passwword is " +password+" 'login successful!' Otherwise, it should log 'Incorrect user details!'");
+}
+login( "2200704408@gmail.com", "22/U/4408/EVE@2024");
